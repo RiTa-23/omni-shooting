f9c42185bc45e67c9c5876abc6c8a760fdf7f0f7
RiTa-23 <rita.tifrt@gmail.com>
2025-01-11 11:53:06 +0900

    [change] 速度アイテム廃止

--- Assets/VSstage/Script/Item.cs
+++ Assets/VSstage/Script/Item.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] AudioClip itemGetSE;
-    [SerializeField] Sprite speedUp_img;
-    [SerializeField] Sprite maxSpeedUp_img;
+    //[SerializeField] Sprite speedUp_img;
+    //[SerializeField] Sprite maxSpeedUp_img;
     [SerializeField] Sprite maxEnergyUp_img;
     [SerializeField] Sprite rapidFireUp_img;
     [SerializeField] Sprite energySpeedUp_img;
@@ -17,14 +17,14 @@ public class Item : MonoBehaviour
     [SerializeField] Sprite invincible_img;
     [SerializeField] Sprite infinity_img;
 
-    [SerializeField] int speedUp_P = 15;
-    [SerializeField] int maxSpeedUp_P = 15;
-    [SerializeField] int maxEnergyUp_P = 15;
-    [SerializeField] int rapidFireUp_P = 15;
-    [SerializeField] int energySpeedUp_P = 15;
-    [SerializeField] int lifeRecovery_P = 15;
-    [SerializeField] int invincible_P = 5;
-    [SerializeField] int infinity_P = 5;
+    //[SerializeField] int speedUp_P = 15;
+    //[SerializeField] int maxSpeedUp_P = 15;
+    int maxEnergyUp_P = 25;
+    int rapidFireUp_P = 25;
+    int energySpeedUp_P = 25;
+    int lifeRecovery_P = 15;
+    int invincible_P = 5;
+    int infinity_P = 5;
 
     // Start is called before the first frame update
 
@@ -37,42 +37,42 @@ public class Item : MonoBehaviour
 
         //乱数からアイテム決定
         //アイテムごとに見た目を変える
-        if (rnd <= speedUp_P)
-        {
-            thisItemName = ItemName.speedUp;
-            spriteRenderer.sprite = speedUp_img;
-        }
-        else if(rnd<=speedUp_P+maxSpeedUp_P)
-        {
-            thisItemName = ItemName.maxSpeedUp;
-            spriteRenderer.sprite = maxSpeedUp_img;
-        }
-        else if(rnd<= speedUp_P + maxSpeedUp_P+ maxEnergyUp_P)
+        //if (rnd <= speedUp_P)
+        //{
+        //    thisItemName = ItemName.speedUp;
+        //    spriteRenderer.sprite = speedUp_img;
+        //}
+        //else if(rnd<=speedUp_P+maxSpeedUp_P)
+        //{
+        //    thisItemName = ItemName.maxSpeedUp;
+        //    spriteRenderer.sprite = maxSpeedUp_img;
+        //}
+        if(rnd<=  maxEnergyUp_P)
         {
             thisItemName = ItemName.maxEnergyUp;
            spriteRenderer.sprite= maxEnergyUp_img;
         }
-        else if(rnd<= speedUp_P + maxSpeedUp_P + maxEnergyUp_P+rapidFireUp_P)
+        else if(rnd<=  maxEnergyUp_P+rapidFireUp_P)
         {
             thisItemName = ItemName.rapidFireUp;
             spriteRenderer.sprite = rapidFireUp_img;
         }
-        else if(rnd<= speedUp_P + maxSpeedUp_P + maxEnergyUp_P + rapidFireUp_P+energySpeedUp_P)
+        else if(rnd<=  maxEnergyUp_P + rapidFireUp_P+energySpeedUp_P)
         {
             thisItemName = ItemName.energySpeedUp;
             spriteRenderer.sprite= energySpeedUp_img;
         }
-        else if(rnd<= speedUp_P + maxSpeedUp_P + maxEnergyUp_P + rapidFireUp_P + energySpeedUp_P+lifeRecovery_P)
+        else if(rnd<=  maxEnergyUp_P + rapidFireUp_P + energySpeedUp_P+lifeRecovery_P)
         {
             thisItemName = ItemName.lifeRecovery;
             spriteRenderer.sprite=lifeRecovery_img;
         }
-        else if(rnd<=speedUp_P + maxSpeedUp_P + maxEnergyUp_P + rapidFireUp_P + energySpeedUp_P + lifeRecovery_P+invincible_P)
+        else if(rnd<= maxEnergyUp_P + rapidFireUp_P + energySpeedUp_P + lifeRecovery_P+invincible_P)
         {
             thisItemName = ItemName.invincible;
             spriteRenderer.sprite = invincible_img;
         }
-        else if(rnd<= speedUp_P + maxSpeedUp_P + maxEnergyUp_P + rapidFireUp_P + energySpeedUp_P + lifeRecovery_P+invincible_P+infinity_P)
+        else if(rnd<=  maxEnergyUp_P + rapidFireUp_P + energySpeedUp_P + lifeRecovery_P+invincible_P+infinity_P)
         {
             thisItemName = ItemName.infinity;
             spriteRenderer.sprite=infinity_img;
@@ -83,16 +83,10 @@ public class Item : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public enum ItemName
     {
-        speedUp,//速度UP
-        maxSpeedUp,//最大速度UP（主にドッジで役立つ）
+        //speedUp,//速度UP
+        //maxSpeedUp,//最大速度UP（主にドッジで役立つ）
         maxEnergyUp,//最大エネルギー上昇
         rapidFireUp,//連射力UP
         energySpeedUp,//エネルギー回復スピードUP
@@ -141,8 +135,8 @@ public class Item : MonoBehaviour
                 //アイテムの効果
                 switch (thisItemName)
                 {
-                    case ItemName.speedUp: playerController.force += 2.5f; break;
-                    case ItemName.maxSpeedUp: playerController.maxSpeed += 2; break;
+                    //case ItemName.speedUp: playerController.force += 2.5f; break;
+                    //case ItemName.maxSpeedUp: playerController.maxSpeed += 2; break;
                     case ItemName.maxEnergyUp: playerStatus.MaxEnergy += 20; break;
                     case ItemName.energySpeedUp: playerStatus.energyNaturalRecovery += 0.04f; break;
                     case ItemName.rapidFireUp:
--- Assets/VSstage/Script/PlayerItem.cs
+++ Assets/VSstage/Script/PlayerItem.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerItem : MonoBehaviour
 {
+    //プレイヤーが現在所持しているアイテムをリスト管理
+
     // Start is called before the first frame update
     public Dictionary<Item.ItemName,int> ItemList;
     void Start()
     {
         ItemList = new Dictionary<Item.ItemName, int>();
-        ItemList.Add(Item.ItemName.speedUp, 0);
-        ItemList.Add(Item.ItemName.maxSpeedUp, 0);
+        //ItemList.Add(Item.ItemName.speedUp, 0);
+        //ItemList.Add(Item.ItemName.maxSpeedUp, 0);
         ItemList.Add(Item.ItemName.maxEnergyUp, 0);
         ItemList.Add(Item.ItemName.energySpeedUp, 0);
         ItemList.Add(Item.ItemName.rapidFireUp, 0);
@@ -24,8 +26,8 @@ public class PlayerItem : MonoBehaviour
 
     public void ResetItemList()
     {
-        ItemList[Item.ItemName.speedUp] = 0;
-        ItemList[Item.ItemName.maxSpeedUp] = 0;
+        //ItemList[Item.ItemName.speedUp] = 0;
+        //ItemList[Item.ItemName.maxSpeedUp] = 0;
         ItemList[Item.ItemName.maxEnergyUp] = 0;
         ItemList[Item.ItemName.energySpeedUp] = 0;
         ItemList[Item.ItemName.rapidFireUp] = 0;
